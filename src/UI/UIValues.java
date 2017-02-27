147bcbdbd80e12db49bea96e5948a36bb01e908a
Anne1231 <at.sisy@gmail.com>
2017-02-27 09:17:19 +0900

    適当に作っていたものをGitで管理することにした

---
+++ src/UI/UIValues.java
@@ -0,0 +1,24 @@
+package UI;
+
+/**
+ * Created by Akihiro on 2017/02/25.
+ */
+public class UIValues {
+
+    static final int WINDOW_WIDTH = 1920;
+    static final int WINDOW_HEIGHT = 1050;
+
+    static final double DOT_CIRCLE_WIDTH = 1;
+
+    static final double FOOTER_WIDTH = WINDOW_WIDTH;
+    static final double FOOTER_HEIGHT = 20;
+
+    static final double MENU_HEIGHT = 25;
+
+    static final double LIST_TO_CANVAS_WIDTH = 10;
+
+    static final double LAYER_LIST_WIDTH = 150.0;
+    static final double LAYER_LIST_HEIGHT = WINDOW_HEIGHT - FOOTER_HEIGHT - MENU_HEIGHT;
+
+
+}
